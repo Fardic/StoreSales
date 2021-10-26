0c92d3ab9b36c38b35e59987d9f6f447dc89f24f
Fardic <fatihozgurardic@gmail.com>
2021-10-26 14:26:00 +0300

    dataset class is written for rnn of oil money

---
+++ dataset.py
@@ -0,0 +1,29 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+from torchvision import transforms
+
+class rnn_custom_dataset(Dataset):
+    
+    def __init__(self, data) -> None:
+        super().__init__()
+        xy = data
+        self.x = xy[:, :, :-1].astype("int32")
+        self.y = xy[:, :, -1].astype("float32")
+        self.n_samples = xy.shape[0]
+        del xy
+        self.transform = transforms.Compose([transforms.ToTensor()])
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+    
+    def __len__(self):
+        return self.n_samples
+
+
+
+
+
+
+
+
